63e78d542dd7075abd911a45348826c06c1bd4f6
Salma Aqarrout <salma.aqarrout@kcl.ac.uk>
2023-12-06 22:32:05 +0000

    Update primes.py

--- primes.py
+++ primes.py
@@ -1,20 +1,22 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
-
 def primes(number_of_primes):
-    if number_of_primes <= 0:
-        raise ValueError(f"number_of_primes= {number_of_primes} should not be negative or 0")
     list = []
-    i = 2
-    while len(list) < number_of_primes:
+    if number_of_primes <= 0:
+        raise ValueError
+    ctr = 2
+    while (len(list) < number_of_primes):
+        ctr2 = 2
         isPrime = True
-        for j in range (2, int(i**0.5) + 1):
-            if i%j == 0:
+        while (ctr2 < ctr):
+            if ctr % ctr2 == 0:
                 isPrime = False
-        if isPrime:
-            list.append(i)
-        i += 1
-    return list
+            ctr2 += 1
 
-print(primes(0))
+        if isPrime:
+            list.append(ctr)
+        
+        ctr+=1
+
+    return list
